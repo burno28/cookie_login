f8b773869d5080ae671f14c0901896e637339600
BONGJIN CHO <bjchomr@naver.com>
2022-12-29 21:43:31 +0900

    로그인 하다만것

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 //노드 정예반 로그인 기능 실습
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cookie = require('cookie')
+
 const app = express();
+app.use(cookieParser());
+
+app.use(express.json())
 const port = 3000;
 
-app.post('/login', (req,res) => {
+const users = [
+    { name: "가나다", id: "abc", password: "1234" },
+    { name: "라마바", id: "def", password: "5678" },
+    { name: "사아자", id: "ghi", password: "9012" },
+]
+
+app.post('/login', (req, res) => {
+    const id = req.body.id
+    const password = req.body.password
+    const user = users.find(user => user, id === id && user.password === password)
+
+    if (!user) {
+        return res.send('who are you?')
+    }
+    console.log("header", 'user_id = ${user.id}')
+    res.writehead(200, {
+        "Set-cookie": ['user_id = ${user.id}']
+    })
 
     res.send('login api page')
 })
 
-app.post('/logout', (req,res) => {
+app.post('/logout', (req, res) => {
 
     res.send('login api page')
 })
 
-app.post('/register', (req,res) => {
+app.post('/register', (req, res) => {
 
     res.send('register api page')
 })
 
-app.get('/users', (req,res) => {
-
+app.get('/users', (req, res) => {
+    console.log(req.cookies.test)
+    console.log(req.cookies.test)
     res.send('users api page')
 })
 
